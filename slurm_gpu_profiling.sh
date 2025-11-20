4d5c441a16fb2dc67aaed899743895464ab100f7
Ian Wallace <ian.wallace2560@gmail.com>
2025-11-19 19:19:31 -0500

    Fix time

--- slurm_gpu_profiling.sh
+++ slurm_gpu_profiling.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:nvidia_h100_pcie:1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=16G
-#SBATCH --time=2:00:00
+#SBATCH --time=00:30:00
 #SBATCH -o batchsize_%j.out
 
 time python profiler.py --all
